3bcd518626bc41217ae354e064e64da064989c54
alejoprograming26 <151877512+alejoprograming26@users.noreply.github.com>
2025-09-05 09:24:37 -0400

    Autorizaciones

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -14,145 +14,167 @@
             <img src="{{ url('/img/gestiones.gif') }}" width="90px" alt="">
             <div class="info-box-content">
                 <span class="info-box-text"><b>Gestiones registrados</b></span>
-                <span class="info-box-number" style="color:rgb(80, 63, 215);font-size:18pt">
+                <span class="info-box-number" style="font-size:18pt">
                     {{ $total_gestiones }} Gestiones
                 </span>
             </div>
         </div>
     </div>
  @endcan
-
-    <div class="col-md-3 col-sm-6 col-12">
+ @can('admin.periodos.index')
+     <div class="col-md-3 col-sm-6 col-12">
         <div class="info-box zoomP">
             <img src="{{ url('/img/periodo.gif') }}" width="90px" alt="">
             <div class="info-box-content">
                 <span class="info-box-text"><b>Periodos Registrados</b></span>
-                <span class="info-box-number" style="color:rgb(80, 63, 215);font-size:18pt">
+                <span class="info-box-number" style="font-size:18pt">
                     {{ $total_periodos }} Periodos
                 </span>
             </div>
         </div>
     </div>
+ @endcan
 
+@can('admin.niveles.index')
     <div class="col-md-3 col-sm-6 col-12">
         <div class="info-box zoomP">
             <img src="{{ url('/img/nivell.gif') }}" width="90px" alt="">
             <div class="info-box-content">
                 <span class="info-box-text"><b>Niveles Registrados</b></span>
-                <span class="info-box-number" style="color:rgb(80, 63, 215);font-size:18pt">
+                <span class="info-box-number" style="font-size:18pt">
                     {{ $total_niveles }} Niveles
                 </span>
             </div>
         </div>
     </div>
+@endcan
 
+
+    @can('admin.grados.index')
     <div class="col-md-3 col-sm-6 col-12">
         <div class="info-box zoomP">
             <img src="{{ url('/img/grado.gif') }}" width="90px" alt="">
             <div class="info-box-content">
                 <span class="info-box-text"><b>Grados Registrados</b></span>
-                <span class="info-box-number" style="color:rgb(80, 63, 215);font-size:18pt">
+                <span class="info-box-number" style="font-size:18pt">
                     {{ $total_grados }} Grados
                 </span>
             </div>
         </div>
     </div>
+    @endcan
 
-    <div class="col-md-3 col-sm-6 col-12">
+    @can('admin.paralelos.index')
+        <div class="col-md-3 col-sm-6 col-12">
         <div class="info-box zoomP">
             <img src="{{ url('/img/paralelo.gif') }}" width="90px" alt="">
             <div class="info-box-content">
                 <span class="info-box-text"><b>Paralelos Registrados</b></span>
-                <span class="info-box-number" style="color:rgb(80, 63, 215);font-size:18pt">
+                <span class="info-box-number" style="font-size:18pt">
                     {{ $total_paralelos }} Paralelos
                 </span>
             </div>
         </div>
     </div>
+    @endcan
 
-    <div class="col-md-3 col-sm-6 col-12">
+    @can('admin.turnos.index')
+        <div class="col-md-3 col-sm-6 col-12">
         <div class="info-box zoomP">
             <img src="{{ url('/img/turno.gif') }}" width="90px" alt="">
             <div class="info-box-content">
                 <span class="info-box-text"><b>Turnos Registrados</b></span>
-                <span class="info-box-number" style="color:rgb(80, 63, 215);font-size:18pt">
+                <span class="info-box-number" style="font-size:18pt">
                     {{ $total_turnos }} Turnos
                 </span>
             </div>
         </div>
     </div>
+    @endcan
 
-    <div class="col-md-3 col-sm-6 col-12">
+   @can('admin.materias.index')
+   <div class="col-md-3 col-sm-6 col-12">
         <div class="info-box zoomP">
             <img src="{{ url('/img/materias.gif') }}" width="90px" alt="">
             <div class="info-box-content">
                 <span class="info-box-text"><b>Materias Registradas</b></span>
-                <span class="info-box-number" style="color:rgb(80, 63, 215);font-size:18pt">
+                <span class="info-box-number" style="font-size:18pt">
                     {{ $total_materias }} Materias
                 </span>
             </div>
         </div>
     </div>
+   @endcan
 
-    <div class="col-md-3 col-sm-6 col-12">
+   @can('admin.roles.index')
+       <div class="col-md-3 col-sm-6 col-12">
         <div class="info-box zoomP">
             <img src="{{ url('/img/roles.gif') }}" width="90px" alt="">
             <div class="info-box-content">
                 <span class="info-box-text"><b>Roles Registrados</b></span>
-                <span class="info-box-number" style="color:rgb(80, 63, 215);font-size:18pt">
+                <span class="info-box-number" style="font-size:18pt">
                     {{ $total_roles }} Roles
                 </span>
             </div>
         </div>
     </div>
+   @endcan
 
-    <div class="col-md-3 col-sm-6 col-12">
+
+  @can('admin.personal.index')
+  <div class="col-md-3 col-sm-6 col-12">
         <div class="info-box zoomP">
             <img src="{{ url('/img/adminis.gif') }}" width="90px" alt="">
             <div class="info-box-content">
                 <span class="info-box-text"><b>Personal Administrativo Registrado</b></span>
-                <span class="info-box-number" style="color:rgb(80, 63, 215);font-size:18pt">
+                <span class="info-box-number" style="font-size:18pt">
                     {{ $total_personal_admin }} Administrativos
                 </span>
             </div>
         </div>
     </div>
 
-    <div class="col-md-3 col-sm-6 col-12">
+     <div class="col-md-3 col-sm-6 col-12">
         <div class="info-box zoomP">
             <img src="{{ url('/img/maestro.gif') }}" width="90px" alt="">
             <div class="info-box-content">
                 <span class="info-box-text"><b>Personal Docente Registrado</b></span>
-                <span class="info-box-number" style="color:rgb(80, 63, 215);font-size:18pt">
+                <span class="info-box-number" style="font-size:18pt">
                     {{ $total_personal_docente }} Docentes
                 </span>
             </div>
         </div>
     </div>
+  @endcan
 
+
+@canany(['admin.estudiantes.index', 'admin.pagos.index'])
     <div class="col-md-3 col-sm-6 col-12">
         <div class="info-box zoomP">
             <img src="{{ url('/img/estudiante.gif') }}" width="90px" alt="">
             <div class="info-box-content">
                 <span class="info-box-text"><b>Estudiantes Registrados</b></span>
-                <span class="info-box-number" style="color:rgb(80, 63, 215);font-size:18pt">
+                <span class="info-box-number" style="font-size:18pt">
                     {{ $total_estudiantes }} Estudiantes
                 </span>
             </div>
         </div>
     </div>
+    @endcan
 
+    @can('admin.ppffs.index')
     <div class="col-md-3 col-sm-6 col-12">
         <div class="info-box zoomP">
             <img src="{{ url('/img/padre.gif') }}" width="90px" alt="">
             <div class="info-box-content">
                 <span class="info-box-text"><b>Padres Registrados</b></span>
-                <span class="info-box-number" style="color:rgb(80, 63, 215);font-size:18pt">
+                <span class="info-box-number" style="font-size:18pt">
                     {{ $total_ppff }} Padres
                 </span>
             </div>
         </div>
     </div>
+    @endcan
 </div>
 <div class="row">
     <div class="col-md-6">
